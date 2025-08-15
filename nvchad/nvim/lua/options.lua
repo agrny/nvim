c25ab403b8e1518598ec09887b1a3481d9da33bb
Alec Greenaway <alecgreenaway1@gmail.com>
2025-08-15 09:32:04 -0700

    added nvchad config

---
+++ nvchad/nvim/lua/options.lua
@@ -0,0 +1,9 @@
+require "nvchad.options"
+
+vim.o.tabstop = 4 -- Number of spaces a tab counts for
+vim.o.shiftwidth = 4 -- Number of spaces for auto-indent
+vim.o.expandtab = true -- Use spaces instead of tab characters
+vim.o.undofile = true
+vim.o.undodir = "~/.local/share/nvim"
+vim.o.swapfile = false
+
