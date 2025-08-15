ca2cb750e8a98cd60a21ad19ec5eb170e4ea83f8
Alec Greenaway <alecgreenaway1@gmail.com>
2025-08-15 10:12:15 -0700

    fixed '~' dir being made on save

--- nvchad/nvim/lua/options.lua
+++ nvchad/nvim/lua/options.lua
@@ -3,7 +3,16 @@ require "nvchad.options"
 vim.o.tabstop = 4 -- Number of spaces a tab counts for
 vim.o.shiftwidth = 4 -- Number of spaces for auto-indent
 vim.o.expandtab = true -- Use spaces instead of tab characters
-vim.o.undofile = true
-vim.o.undodir = "~/.local/share/nvim"
-vim.o.swapfile = false
+
+local backupdir = vim.fn.stdpath("data") .. "/backup//"
+vim.opt.backupdir = backupdir
+vim.fn.mkdir(backupdir, "p")
+
+local swapdir = vim.fn.stdpath("data") .. "/swap//"
+vim.opt.directory = swapdir
+vim.fn.mkdir(swapdir, "p")
+
+local undodir = vim.fn.stdpath("data") .. "/undo//"
+vim.opt.undodir = undodir
+vim.fn.mkdir(undodir, "p")
 
