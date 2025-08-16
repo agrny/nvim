9a2b6790a85de296f84c1ea78cbb56fbf664e066
Alec Greenaway <alecgreenaway1@gmail.com>
2025-08-15 18:00:03 -0700

    add mapping for opencode

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -10,3 +10,29 @@ vim.keymap.set("x", "<leader>rr", [[:s/<C-r><C-w>//g<Left><Left>]], { noremap = 
 vim.keymap.set("x", "<leader>R", [[:<C-u>%s/<C-r>0//g<Left><Left>]], { noremap = true, silent = false
 })
 
+
+vim.keymap.set("n", "<leader>e", ":NvimTreeToggle<CR>", { noremap = true, silent = true , desc=""})
+
+-- -- Open a new Zellij pane and run opencode in the current working directory
+vim.keymap.set("n", "<leader>o", function()
+  -- local cwd = vim.fn.getcwd()
+  vim.fn.jobstart(
+    { "zellij", "action", "new-pane", "--cwd" ,"$(pwd)", "--" ,"opencode" },
+    { detach = true }
+  )
+end, { noremap = true, silent = true })
+
+
+-- TODO: Opens new tab but doesnt run command in it
+-- vim.keymap.set("n", "<leader>O", function()
+--   -- local cwd = vim.fn.getcwd()
+--   vim.fn.jobstart(
+--     { "zellij", "action", "new-tab" },
+--     { detach = true }
+--   )
+--  vim.fn.jobstart({ "zellij", "action", "focus-next-tab" }, { detach = true })
+--   vim.fn.jobstart(
+--     { "zellij", "action", "run" , "--command", "opencode"} ,
+--     { detach = true }
+--   )
+-- end, { noremap = true, silent = true })
