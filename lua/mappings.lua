b92a06e13a87df550b46a03c33b68d49b995578b
Alec Greenaway <alecgreenaway1@gmail.com>
2025-08-15 20:30:31 -0700

    add copilot

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -1,27 +1,31 @@
-require "nvchad.mappings"
+require("nvchad.mappings")
 local map = vim.keymap.set
 
 -- map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
 
 map("n", ";", ":", { desc = "CMD enter command mode" })
 map("i", "jk", "<ESC>")
-vim.keymap.set("x", "<leader>rr", [[:s/<C-r><C-w>//g<Left><Left>]], { noremap = true, silent = false , desc="replace in visual selection"})
+-- Copilot Suggestion Acceptance Key
+map("i", "<C-l>", function()
+	vim.fn.feedkeys(vim.fn["copilot#Accept"](), "")
+end, { desc = "Copilot Accept", noremap = true, silent = true })
+
+vim.keymap.set(
+	"x",
+	"<leader>rr",
+	[[:s/<C-r><C-w>//g<Left><Left>]],
+	{ noremap = true, silent = false, desc = "replace in visual selection" }
+)
 -- Visual mode: replace selection globally in file
-vim.keymap.set("x", "<leader>R", [[:<C-u>%s/<C-r>0//g<Left><Left>]], { noremap = true, silent = false
-})
+vim.keymap.set("x", "<leader>R", [[:<C-u>%s/<C-r>0//g<Left><Left>]], { noremap = true, silent = false })
 
+vim.keymap.set("n", "<leader>e", ":NvimTreeToggle<CR>", { noremap = true, silent = true, desc = "" })
 
-vim.keymap.set("n", "<leader>e", ":NvimTreeToggle<CR>", { noremap = true, silent = true , desc=""})
-
--- -- Open a new Zellij pane and run opencode in the current working directory
+-- Open a new Zellij pane and run opencode in the current working directory
 vim.keymap.set("n", "<leader>o", function()
-  -- local cwd = vim.fn.getcwd()
-  vim.fn.jobstart(
-    { "zellij", "action", "new-pane", "--cwd" ,"$(pwd)", "--" ,"opencode" },
-    { detach = true }
-  )
+	-- local cwd = vim.fn.getcwd()
+	vim.fn.jobstart({ "zellij", "action", "new-pane", "--cwd", "$(pwd)", "--", "opencode" }, { detach = true })
 end, { noremap = true, silent = true })
-
 
 -- TODO: Opens new tab but doesnt run command in it
 -- vim.keymap.set("n", "<leader>O", function()
--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -1,28 +1,36 @@
 return {
-  {
-    "stevearc/conform.nvim",
-    -- event = 'BufWritePre', -- uncomment for format on save
-    opts = require "configs.conform",
-  },
+	{
+		"stevearc/conform.nvim",
+		-- event = 'BufWritePre', -- uncomment for format on save
+		opts = require("configs.conform"),
+	},
 
-  -- These are some examples, uncomment them if you want to see them work!
-  {
-    "neovim/nvim-lspconfig",
-    config = function()
-      require "configs.lspconfig"
-    end,
-  },
+	-- These are some examples, uncomment them if you want to see them work!
+	{
+		"neovim/nvim-lspconfig",
+		config = function()
+			require("configs.lspconfig")
+		end,
+	},
 
-  -- test new blink
-  -- { import = "nvchad.blink.lazyspec" },
+	{
+		"github/copilot.vim",
+		lazy = false,
+		config = function()
+			vim.g.copilot_no_tab_map = true
+			vim.g.copilot_assume_mapped = true
+		end,
+	},
+	-- test new blink
+	-- { import = "nvchad.blink.lazyspec" },
 
-  -- {
-  -- 	"nvim-treesitter/nvim-treesitter",
-  -- 	opts = {
-  -- 		ensure_installed = {
-  -- 			"vim", "lua", "vimdoc",
-  --      "html", "css"
-  -- 		},
-  -- 	},
-  -- },
+	-- {
+	-- 	"nvim-treesitter/nvim-treesitter",
+	-- 	opts = {
+	-- 		ensure_installed = {
+	-- 			"vim", "lua", "vimdoc",
+	--      "html", "css"
+	-- 		},
+	-- 	},
+	-- },
 }
